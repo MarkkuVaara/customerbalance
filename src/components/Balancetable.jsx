480e754fd3c3b4fa702f94e8a294b443564ff5bd
Markku Vaara <markku.vaara45@gmail.com>
2023-08-17 00:47:47 +0300

    Refined table style

--- src/components/Balancetable.jsx
+++ src/components/Balancetable.jsx
@@ -18,27 +18,29 @@ const Balancetable = (props) => {
         <>
         {balance.map(balanceunit => 
             <div className="balance" key={balanceunit.id}>
-                {balanceunit.userId === 1 && <p>January</p>}
-                {balanceunit.userId === 2 && <p>February</p>}
-                {balanceunit.userId === 3 && <p>March</p>}
-                {balanceunit.userId === 4 && <p>April</p>}
-                {balanceunit.userId === 5 && <p>May</p>}
-                {balanceunit.userId === 6 && <p>June</p>}
-                {balanceunit.userId === 7 && <p>July</p>}
-                {balanceunit.userId === 8 && <p>August</p>}
-                {balanceunit.userId === 9 && <p>September</p>}
-                {balanceunit.userId === 10 && <p>October</p>}
-                {balanceunit.userId === 11 && <p>November</p>}
-                {balanceunit.userId === 12 && <p>December</p>}
-                <p>{Math.floor(Math.random() * 30 + 1)}th</p>
-                <p><b>{balanceunit.id}</b></p>
-                <p>{balanceunit.title}</p>
+                {balanceunit.userId === 1 && <p className="balancemonth">January</p>}
+                {balanceunit.userId === 2 && <p className="balancemonth">February</p>}
+                {balanceunit.userId === 3 && <p className="balancemonth">March</p>}
+                {balanceunit.userId === 4 && <p className="balancemonth">April</p>}
+                {balanceunit.userId === 5 && <p className="balancemonth">May</p>}
+                {balanceunit.userId === 6 && <p className="balancemonth">June</p>}
+                {balanceunit.userId === 7 && <p className="balancemonth">July</p>}
+                {balanceunit.userId === 8 && <p className="balancemonth">August</p>}
+                {balanceunit.userId === 9 && <p className="balancemonth">September</p>}
+                {balanceunit.userId === 10 && <p className="balancemonth">October</p>}
+                {balanceunit.userId === 11 && <p className="balancemonth">November</p>}
+                {balanceunit.userId === 12 && <p className="balancemonth">December</p>}
+                <p className="balancedate">{Math.floor(Math.random() * 30 + 1)}th</p>
+                <p className="balancetransaction"><b>{balanceunit.id} e</b></p>
+                <p className="balancedescription">{balanceunit.title}</p>
             </div>
         )}
+        <br></br>
         <button onClick={() => countBalance()}>Calculate cumulative balance</button>
         <div>
-            <h2>{cumulBalance}</h2>
+            <h2>{cumulBalance} euros</h2>
         </div>
+        <br></br>
         </>
     )
 };
