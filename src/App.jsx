7581d817bfb39aa7ead04fa01852bd59c541b06b
Markku Vaara <markku.vaara45@gmail.com>
2023-08-16 18:40:35 +0300

    Added axios and service for balance fetching

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,21 @@ import React, { useState, useEffect } from 'react';
 
 import Title from './components/Title';
 import Banner from './components/Banner';
+import Balanceservice from './services/Balances';
 
 const App = () => {
 
-  const [balance, setBalance] = useState(0);
+  const [balance, setBalance] = useState([]);
   
   useEffect(() => {
     console.log('Fetching..');
-  });
+    Balanceservice
+      .getAll()
+      .then(response => {
+        console.log('Balance fetched..');
+        setBalance(response.data);
+      });
+  }, []);
 
   return (
 
@@ -18,7 +25,10 @@ const App = () => {
       <Title />
       <Banner />
       <div className="main">
-        <p>Hello, your balance is {balance}.</p>
+        {balance.map(balanceunit => 
+          <p key={balanceunit.id}>Hello {balanceunit.id}, your balance is {balanceunit.title}.</p>
+        )}
+        <button>Fetch your balance</button>
       </div>
     </div>
 
