125969f4162782746b2440160718325295eb3fcd
Markku Vaara <markku.vaara45@gmail.com>
2023-10-14 23:13:59 +0300

    Modified and refined personal information

--- src/components/Personalinfo.jsx
+++ src/components/Personalinfo.jsx
@@ -1,33 +1,50 @@
 
 import House from '../images/house.jpg';
+import Email from '../images/email.jpg';
+import Phone from '../images/phone.jpg';
+import Bank from '../images/bank.jpg';
+import Info from '../images/info.jpg';
 
 const Personalinfo = () => {
 
     return (
         <div className="info">
             <h3>MARKKU TAPIO VAARA</h3>
-            <div>
+            <div className="subinfo">
                 <img src={House}></img>
-                <b>Osoite</b>
-                <p>Kullerovuorentie 9 R 52</p>
-                <p>90900  Fingerpori</p>
+                <div>
+                    <b>Osoite</b>
+                    <p>Kullerovuorentie 9 R 52 90900 Fingerpori</p>
+                </div>
             </div>
-            <div>
-                <b>Sähköpostiosoite</b>
-                <p>markku.vaara@customer.com</p>
+            <div className="subinfo">
+                <img src={Email}></img>
+                <div>
+                    <b>Sähköpostiosoite</b>
+                    <p>markku.vaara@customer.com</p>
+                </div>
             </div>
-            <div>
-                <b>Puhelinnumero</b>
-                <p>0500 111 222</p>
+            <div className="subinfo">
+                <img src={Phone}></img>
+                <div>
+                    <b>Puhelinnumero</b>
+                    <p>0500 111 222</p>
+                </div>
             </div>
-            <div>
-                <b>Asiakkuus</b>
-                <p>Fingerporin Monetarum</p>
+            <div className="subinfo">
+                <img src={Bank}></img>
+                <div>
+                    <b>Asiakkuus</b>
+                    <p>Fingerporin Monetarum</p>
+                </div>
             </div>
-            <div>
-                <b>Asiakastiedot</b>
-                <br/>
-                <button>Muokkaa</button>
+            <div className="subinfo">
+                <img src={Info}></img>
+                <div>
+                    <b>Asiakastiedot</b>
+                    <br/>
+                    <button>Muokkaa</button>
+                </div>
             </div>
         </div>
     )
