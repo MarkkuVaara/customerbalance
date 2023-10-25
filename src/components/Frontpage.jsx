559b06e737846f98d6c056d978c4a80811a4919e
Markku Vaara <markku.vaara45@gmail.com>
2023-10-26 01:25:06 +0300

    Modified frontpage and loan page

--- src/components/Frontpage.jsx
+++ src/components/Frontpage.jsx
@@ -5,10 +5,11 @@ const Frontpage = () => {
 
     return (
         <div className="frontpage">
-            <h3>Tervetuloa Monetarum pankkiin!</h3>
-            <p>Olemme Suomen paras ja turvallisin pankki.</p>
-            <p>Tarjoamme parhaat talletuskorot ja kilpailukykyisimmät lainat.</p>
+            <h3>Tervetuloa Monetarum-pankkiin!</h3>
             <br />
+            <p>Monetarumilla on kattava konttoriverkosto Ruotsissa, Suomessa, Norjassa ja Alankomaissa. Monetarum on perustettu vuonna 1971.</p>
+            <p>Pankilla on yli 2 000 työntekijää. Vuonna 2019 pankkimme esihenkilöistä 40 prosenttia oli naisia. Toimintatapamme on hajautettu, ja toiminnan keskiössä ovat aina asiakkaan tarpeet.</p>
+            <p>Monetarum on vuosia ollut asiakastyytyväisyyden, vastuullisuuden ja luotettavuuden kärkikolmikossa. Pankkimme on myös yksi Euroopan kustannustehokkaimmista ja vakaimmista pörssinoteeratuista pankeista.</p>
             <br />
             <img src={Bankclerk}></img>
         </div>
--- src/components/Loans.jsx
+++ src/components/Loans.jsx
@@ -1,9 +1,16 @@
 
+import Coins from '../images/coins.jpg';
+
 const Loans = () => {
 
     return (
-        <div>
+        <div className="loans">
             <h3>Lainat</h3>
+            <button><img src={Coins} alt={Coins}></img>Ota asuntolaina</button>
+            <button><img src={Coins} alt={Coins}></img>Ota autolaina</button>
+            <button><img src={Coins} alt={Coins}></img>Ota kulutusluotto</button>
+            <button><img src={Coins} alt={Coins}></img>Ota VISA</button>
+            <button><img src={Coins} alt={Coins}></img>Ota multa rahaa</button>
         </div>
     )
 
