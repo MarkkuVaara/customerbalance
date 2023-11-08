7a4df58d3f92c487ae03a609edc4c2a9c15e5c56
Markku Vaara <markku.vaara45@gmail.com>
2023-11-08 23:11:59 +0200

    Created better buttons and button images for loans

--- src/components/Loans.jsx
+++ src/components/Loans.jsx
@@ -1,19 +1,35 @@
 
-import Coins from '../images/coins.jpg';
-import House from '../images/house.jpg';
-import Car from '../images/car.jpg';
-
 const Loans = (props) => {
 
     return (
-        <span className="loans">
+        <div className="loans">
             <h3>Lainat</h3>
-            <button onClick={props.takeLoan}><img height={70} src={House} alt={Coins}></img>Ota asuntolaina</button>
-            <button onClick={props.takeLoan}><img height={70} src={Car} alt={Coins}></img>Ota autolaina</button>
-            <button onClick={props.takeLoan}><img src={Coins} alt={Coins}></img>Ota kulutusluotto</button>
-            <button onClick={props.takeLoan}><img src={Coins} alt={Coins}></img>Ota VISA</button>
-            <button onClick={props.takeLoan}><img src={Coins} alt={Coins}></img>Ota multa rahaa</button>
-        </span>
+            <span className="houseloan">
+                <button onClick={props.takeLoan}>
+                    <p>Asuntolaina</p>
+                </button>
+            </span>
+            <span className="carloan">
+                <button onClick={props.takeLoan}>
+                    <p>Autolaina</p>
+                </button>
+            </span>
+            <span className="loan">
+                <button onClick={props.takeLoan}>
+                    <p>Kulutusluotto</p>
+                </button>
+            </span>
+            <span className="visaloan">
+                <button onClick={props.takeLoan}>
+                    <p>VISA</p>
+                </button>
+            </span>
+            <span className="moneyloan">
+                <button onClick={props.takeLoan}>
+                    <p>Muu laina</p>
+                </button>
+            </span>
+        </div>
     )
 
 };
