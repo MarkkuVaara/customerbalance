9e2a275106639514a14a71389c9979bb9aca7786
Markku Vaara <markku.vaara45@gmail.com>
2023-11-09 22:54:43 +0200

    Refined loan buttons and changed text font size

--- src/components/Loans.jsx
+++ src/components/Loans.jsx
@@ -3,32 +3,54 @@ const Loans = (props) => {
 
     return (
         <div className="loans">
+            <br />
             <h3>Lainat</h3>
+            <p>Kun tarvitset lainaa, löydät sopivan vaihtoehdon meiltä.</p>
+            <p>Voit hakea lainaa asunnon tai kesämökin hankintaan, auton tai veneen ostoon, remontointiin tai vaikka opintojen rahoittamiseen.</p>
             <span className="houseloan">
                 <button onClick={props.takeLoan}>
-                    <p>Asuntolaina</p>
+                    Asuntolaina
                 </button>
             </span>
             <span className="carloan">
                 <button onClick={props.takeLoan}>
-                    <p>Autolaina</p>
+                    Autolaina
+                </button>
+            </span>
+            <span className="studentloan">
+                <button onClick={props.takeLoan}>
+                    Opintolaina
                 </button>
             </span>
             <span className="loan">
                 <button onClick={props.takeLoan}>
-                    <p>Kulutusluotto</p>
+                    Kulutusluotto
                 </button>
             </span>
             <span className="visaloan">
                 <button onClick={props.takeLoan}>
-                    <p>VISA</p>
+                    VISA
                 </button>
             </span>
             <span className="moneyloan">
                 <button onClick={props.takeLoan}>
-                    <p>Muu laina</p>
+                    Muu laina
                 </button>
             </span>
+            <br />
+            <h3>Lainan haku</h3>
+            <p>Voit hakea ja saada meiltä vakuudellista lainaa, vaikka et vielä olisi asiakkaamme. 
+                Hakemuksen voit täyttää verkossa myös muun pankin tunnuksilla. Vakuudetonta kulutusluottoa 
+                voit saada vain, jos olet pääasiallinen asiakas.</p>
+            <p>Helpoin tapa selvittää, saisitko lainaa, on tehdä lainahakemus. Yleisesti ottaen lainan 
+                myöntämiseen vaikuttaa muun muassa taloudellinen tilanteesi.</p>
+            <h3>Lainan määrä</h3>
+            <p>Se kuinka paljon voit saada lainaa on monen asian summa. Hae lainaa, kun olet tutustunut 
+                lainan myöntämiseen vaikuttaviin tekijöihin. Lainan saantiin vaikuttavat muun muassa: 
+                Kuinka paljon lainaa olet hakemassa suhteessa kaikkiin tuloihisi ja menoihisi,
+                mahdolliset vakuudet, elämäntilanteesi, ja kuinka olet hoitanut raha-asiasi.</p>
+            <p>Voit hakea vakuudellista lainaa 10 000 eurosta ylöspäin ilman maksimiylärajaa. 
+                Vakuudetonta lainaa voit hakea tarpeestasi riippuen 2 000 - 15 000 euroa.</p>
         </div>
     )
 
