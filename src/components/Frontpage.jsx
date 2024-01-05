14c18c6888b7879b0f5c74eaeb33a4f09777ef74
Markku Vaara <markku.vaara45@gmail.com>
2024-01-05 15:29:19 +0200

    Refined front page and image

--- src/components/Frontpage.jsx
+++ src/components/Frontpage.jsx
@@ -6,12 +6,14 @@ const Frontpage = () => {
     return (
         <div className="frontpage">
             <h3>Tervetuloa Monetarum-pankkiin!</h3>
-            <br />
-            <p>Monetarumilla on kattava konttoriverkosto Ruotsissa, Suomessa, Norjassa ja Alankomaissa. Monetarum on perustettu vuonna 1971.</p>
-            <p>Pankilla on yli 2 000 työntekijää. Vuonna 2019 pankkimme esihenkilöistä 40 prosenttia oli naisia. Toimintatapamme on hajautettu, ja toiminnan keskiössä ovat aina asiakkaan tarpeet.</p>
-            <p>Monetarum on vuosia ollut asiakastyytyväisyyden, vastuullisuuden ja luotettavuuden kärkikolmikossa. Pankkimme on myös yksi Euroopan kustannustehokkaimmista ja vakaimmista pörssinoteeratuista pankeista.</p>
-            <br />
-            <img src={Bankclerk}></img>
+            <div className="fronttext">
+                <div>
+                    <p>Monetarumilla on kattava konttoriverkosto Ruotsissa, Suomessa, Norjassa ja Alankomaissa. Monetarum on perustettu vuonna 1971.</p>
+                    <p>Pankilla on yli 2 000 työntekijää. Vuonna 2019 pankkimme esihenkilöistä 40 prosenttia oli naisia. Toimintatapamme on hajautettu, ja toiminnan keskiössä ovat aina asiakkaan tarpeet.</p>
+                    <p>Monetarum on vuosia ollut asiakastyytyväisyyden, vastuullisuuden ja luotettavuuden kärkikolmikossa. Pankkimme on myös yksi Euroopan kustannustehokkaimmista ja vakaimmista pörssinoteeratuista pankeista.</p>
+                </div>
+                <img src={Bankclerk}></img>
+            </div>
         </div>
     )
 
