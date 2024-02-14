ae9910988fa0d7ee0f84880e91bc143d9975af25
Markku Vaara <markku.vaara45@gmail.com>
2024-02-14 14:00:12 +0200

    Added working api endpoint and some filtering for chatbox

--- src/components/ChatComponent.jsx
+++ src/components/ChatComponent.jsx
@@ -15,10 +15,12 @@ const ChatComponent = () => {
 
     const API_KEY = process.env.REACT_APP_OPENAI_API_KEY;
 
+    const subject = 'banking';
+
     const response = await axios.post(
         'https://api.openai.com/v1/completions', {
-        model: 'text-davinci-003',
-        prompt: userInput,
+        model: 'gpt-3.5-turbo-instruct',
+        prompt: `Please provide information about ${subject}: ${userInput}`,
         max_tokens: 50
      }, {
         headers: {
@@ -27,6 +29,7 @@ const ChatComponent = () => {
       }
     );
 
+    console.log(response.data['usage']);
     const message = response.data.choices[0].text;
     setChatHistory([...chatHistory, { user: userInput, bot: message }]);
     setUserInput('');
